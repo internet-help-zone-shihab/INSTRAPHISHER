dbe9245952e9c9be2ebd6c3c4b3f36a859f2f94f
Shihab <67054762+internet-help-zone-shihab@users.noreply.github.com>
2020-07-16 03:47:54 +0600

    Update shihab.sh

--- shihab.sh
+++ shihab.sh
@@ -121,3 +121,25 @@ sleep 2
 printf "\e[0m\e[1;91m [\e[1;97m~\e[1;91m]\e[1;93m Type \e[1;96mbash instraphisher \e[1;93mto run INSTRAPHISHER !! \e[0m\n"
 printf "\e[0m\n"
 printf "\e[0m\n"
+
+menu() {
+
+clear
+usearname : "internet"
+password  : "shihab"
+
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\033[1;96m[☆] \x1b[1;97mUSER ID \x1b[1;96m>>>> ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;96m[☆] \x1b[1;97mPASWORD \x1b[1;96m>>>> ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username
+            loop = 'false'
+        else:
+            print "Wrong Password"
+            os.system('xdg-open https://www.Youtube.com/UCM4wF_X6IV4SNRumm4N_3IA')
+    else:
+        print "Wrong Username"
+        os.system('xdg-open https://www.Youtube.com/UCM4wF_X6IV4SNRumm4N_3IA')
